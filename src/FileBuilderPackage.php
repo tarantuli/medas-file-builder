65ddb10bfdbd37ecb8a8180be62bc69394d84b0e
Patrick Morpey <patrick@morphp.nl>
2022-01-21 15:39:37 +0100

    Updated packages

--- src/FileBuilderPackage.php
+++ src/FileBuilderPackage.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Medas\FileBuilder;
 
 use Medas\PhpFormatter\PhpFormatterPackage;
+use Medas\ServiceManager\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
 class FileBuilderPackage extends BasePackage
 {
+    use AsSingleton;
+
     public function dependencies(): array
     {
         return $this->dependenciesByClass([
-            PhpFormatterPackage::class
+            PhpFormatterPackage::class,
         ]);
     }
 
