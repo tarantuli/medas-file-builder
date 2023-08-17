7d4e7f6ca7b5099b04d58985c3a45ad09234069a
Patrick Morpey <patrick@morphp.nl>
2023-08-17 09:42:52 +0200

    Updated bootstrapping

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -2,3 +2,17 @@
 
 declare(strict_types=1);
 
+use Medas\FileBuilder\FileBuilderPackage;
+use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+
+chdir(__DIR__);
+
+new ServiceManager(function (): ServiceConfig {
+    $config = new ServiceConfig();
+
+    $config->addPackages([
+        FileBuilderPackage::instance(),
+    ]);
+
+    return $config;
+});
